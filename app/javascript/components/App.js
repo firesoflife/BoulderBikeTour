e18fe46442a8f247b3accb74c5c58386b952a5fa
firesoflife <bryangoertz@gmail.com>
2021-07-13 12:08:12 -0400

    first commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './Home';
+import Countdown from './Countdown';
+
+const App = () => {
+	return (
+		<div>
+			<Home />
+			<Countdown />
+		</div>
+	);
+};
+
+export default App;
