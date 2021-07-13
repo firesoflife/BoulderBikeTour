ec3d1bb2eecc8d3a57163a404f256f11f989a488
firesoflife <bryangoertz@gmail.com>
2021-07-13 12:28:42 -0400

    set up demo map with general styling

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Countdown from './Countdown';
+import Map from './Map';
 
 const App = () => {
 	return (
 		<div>
 			<Home />
 			<Countdown />
+			<Map />
 		</div>
 	);
 };
